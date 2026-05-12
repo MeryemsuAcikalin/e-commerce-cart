b784831f3d09ecd17ae84f08fa8b574a9041e2fa
MeryemsuAcikalin <acikalin.meryemsu@gmail.com>
2026-05-12 17:06:44 +0300

    feat: add initial e-commerce-cart code

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import legacy.OldCart;
+
+public class Main {
+    public static void main(String[] args) {
+        OldCart sepet = new OldCart();
+        sepet.urunEkle("Camera", 7850);
+        sepet.urunEkle("Laptop", 35000);
+        sepet.urunEkle("Mouse", 1200);
+        sepet.sepetGoruntule();
+        double toplamTutar = sepet.odenecekTutar("Ogrenci");
+
+        System.out.println("Toplam tutar: " + toplamTutar + "TL");
+
+    }
+
+}

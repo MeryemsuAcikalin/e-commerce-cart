f53d342e9089c71b8c64b5a682147f43d2533ef9
MeryemsuAcikalin <acikalin.meryemsu@gmail.com>
2026-05-12 17:42:48 +0300

    feat: apply Factory Method for product creation

---
+++ src/factory/CameraFactory.java
@@ -0,0 +1,12 @@
+package factory;
+
+import model.Product;
+
+public class CameraFactory implements ProductFactory {
+
+    @Override
+    public Product urunOlustur() {
+        return new Product("Camera", 7850);
+    }
+
+}
